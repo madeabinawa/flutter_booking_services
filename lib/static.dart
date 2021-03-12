13efaa07276264c5951a24756f8e7d486d4faee5
I Made Dwija Abinawa <madeabinawa@gmail.com>
2021-03-12 10:45:07 +0800

    add static value and theme config

---
+++ lib/static.dart
@@ -0,0 +1,2 @@
+const String apiURL = "booking-services.herokuapp.com";
+const String apiPath = "/api/v1/";
---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+double defaultMargin = 24;
+
+Color mainColor = Color(0xff3B3DBF);
+Color secondaryColor = Color(0xFFCDCEFF);
