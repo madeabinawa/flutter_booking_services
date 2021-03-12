f87e35e568687e26bc9cad2f7ab03d445ebdf874
I Made Dwija Abinawa <madeabinawa@gmail.com>
2021-03-12 21:28:01 +0800

    update theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 double defaultMargin = 24;
 
-Color mainColor = Color(0xff3B3DBF);
+Color mainColor = Color(0xff5843BE);
 Color secondaryColor = Color(0xFFCDCEFF);
+Color blackColor = Color(0xff000000);
+Color greyColor = Color(0xff82868E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins()
+    .copyWith(fontSize: 24, fontWeight: FontWeight.w500, color: blackColor);
+
+TextStyle greyTextStyle = GoogleFonts.poppins()
+    .copyWith(fontSize: 16, fontWeight: FontWeight.w300, color: greyColor);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins()
+    .copyWith(fontSize: 16, fontWeight: FontWeight.w300, color: mainColor);
