67ea68e8ebf73d74a192e8da8f9149f419f16e27
I Made Dwija Abinawa <madeabinawa@gmail.com>
2021-03-12 10:45:44 +0800

    create customer_api

---
+++ lib/api/apis.dart
@@ -0,0 +1,8 @@
+import 'dart:convert';
+
+import 'package:booking_services/models/customer_model.dart';
+import 'package:booking_services/static.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'customer_api.dart';
---
+++ lib/api/customer_api.dart
@@ -0,0 +1,99 @@
+part of 'apis.dart';
+
+class CustomerApi {
+  // * USER LOGIN API
+  static Future<CustomerApiResponse?> login(
+      String email, String password) async {
+    Customer? customer;
+    Uri uri = Uri.http(apiURL, apiPath + 'login');
+
+    final response =
+        await http.post(uri, body: {'email': email, 'password': password});
+
+    if (response.statusCode != 200) {
+      return CustomerApiResponse(message: 'Oops, something went wrong');
+    }
+    final data = jsonDecode(response.body);
+    customer = Customer.fromJson(data);
+
+    // * STORE USER TOKEN TO SHARED PREFERENCES
+    await storeUserToken(customer.data?.token);
+    print('User Token Saved');
+
+    return CustomerApiResponse(
+        customer: customer, message: customer.meta?.message);
+  }
+
+  //* GET USER DETAILS DATA
+  static Future getUserData() async {
+    Meta? meta;
+    String? token = await getUserToken();
+    Uri uri = Uri.http(apiURL, apiPath + 'customer/show');
+
+    final response = await http.get(uri, headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response.statusCode != 200) {
+      return CustomerApiResponse(message: 'Oops, something went wrong');
+    }
+    final data = jsonDecode(response.body)['meta'];
+    meta = Meta.fromJson(data);
+
+    return CustomerApiResponse(message: meta.message);
+  }
+
+  //* USER LOGOUT
+  static Future<CustomerApiResponse?> logout() async {
+    Meta? meta;
+    String? token = await getUserToken();
+    Uri uri = Uri.http(apiURL, apiPath + 'logout');
+
+    final response = await http.get(uri, headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response.statusCode != 200) {
+      return CustomerApiResponse(message: 'Oops, something went wrong');
+    }
+
+    final data = jsonDecode(response.body)['meta'];
+    meta = Meta.fromJson(data);
+
+    return CustomerApiResponse(message: meta.message);
+  }
+
+  //* STORE USER TOKEN TO SHARED PREFERENCES
+  static Future<String?> storeUserToken(String? token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('token', token!);
+  }
+
+  //* GET USER TOKEN FROM SHARED PREFERENCES
+  static Future<String?> getUserToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String? token = prefs.getString('token');
+    return token;
+  }
+
+  //* DESTROY USER TOKEN FROM SHARED PREFERENCES
+  static Future<String> destroyUserToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    await prefs.remove('token');
+    return 'Succesfully revoked token';
+  }
+}
+
+class CustomerApiResponse {
+  final Customer? customer;
+  final String? message;
+
+  CustomerApiResponse({this.customer, this.message});
+}
